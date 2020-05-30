bdcaddb94ac35073c6b81b56c07765af37f49b4a
韩荣 <han-r16@mails.tsinghua.edu.cn>
2020-05-30 21:36:36 +0800

    SimCLR

--- loss/margin_triplet.py
+++ loss/margin_triplet.py
@@ -66,6 +66,7 @@ class MarginTripletLoss(torch.nn.Module):
         #     if losses[i].item() < 0:
         #         losses[i] = torch.tensor(0)
 
+        zero = torch.zeros_like(losses)
         losses = torch.where(losses < 0.0, zero, losses)
         losses = torch.sum(losses)
         return loss / (2 * self.batch_size)
