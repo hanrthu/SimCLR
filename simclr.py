a36b57b457b3a8065c69d434f0acee822a6bb9a0
韩荣 <han-r16@mails.tsinghua.edu.cn>
2020-05-30 15:03:02 +0800

    SimCLR

--- simclr.py
+++ simclr.py
@@ -227,7 +227,7 @@ class SimCLR(object):
 
         top1_acc = 100.0 * top1 / total
         top5_acc = 100.0 * top5 / total
-        print("Top1 Accuracy: %d %" %(top1_acc))
-        print("Top5 Accuracy: %d %" %(top5_acc))
+        print("Top1 Accuracy: %d \%" %(top1_acc))
+        print("Top5 Accuracy: %d \%" %(top5_acc))
 
         model.train()
