df02bdde234994f463dfaca4b26ffc71fbd0014c
韩荣 <han-r16@mails.tsinghua.edu.cn>
2020-05-31 15:22:10 +0800

    SimCLR

--- logistic_evaluator.py
+++ logistic_evaluator.py
@@ -163,7 +163,11 @@ class LogisticRegression(nn.Module):
     
     def __init__(self, n_features, n_classes):
         super(LogisticRegression, self).__init__()
-        self.model = nn.Linear(n_features, n_classes)
+        self.l1 = nn.Linear(n_features, n_features/2)
+        self.l2 = nn.Linear(num_features/2, n_classes)
 
     def forward(self, x):
-        return self.model(x)
+        x = self.l1(x)
+        x = F.relu(x)
+        x = self.l2(x)
+        return x
